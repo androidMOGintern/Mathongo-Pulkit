3a74aba63c9c6294662833fcbc113ef28f88affe
pulkit-mac <aggarwalpulkit596@gmail.com>
2018-02-24 15:20:03 +0530

    Notifications Final AND PAYU FINAL

--- app/src/main/java/com/learnacad/learnacad/Activities/CheckoutActivity.java
+++ app/src/main/java/com/learnacad/learnacad/Activities/CheckoutActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +26,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
+import butterknife.BindView;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 import static android.content.ContentValues.TAG;
@@ -35,12 +37,15 @@ public class CheckoutActivity extends AppCompatActivity {
 
     DatabaseReference myRootref;
     private Integer Coins;
+    TextInputLayout checkout_textinputlayout;
+    Integer amount;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+        checkout_textinputlayout = findViewById(R.id.checkout_textinputlayout);
 
         myRootref = FirebaseDatabase.getInstance().getReference("users/" + getIntent().getStringExtra("studentid"));
         myRootref.addValueEventListener(new ValueEventListener() {
@@ -55,54 +60,61 @@ public class CheckoutActivity extends AppCompatActivity {
             }
 
         });
+        checkout_textinputlayout.getEditText().getText().toString();
     }
 
     public void setupPayUMoney(View view) {
 
-        String txnId = System.currentTimeMillis() + "";
-        String phone = "9582054664";
-        String productName = "Coins";
-        String firstName = "Pulkit Aggarwal";
-        String email = "aggarwalpulkit596@gmail.com";
-        String udf1 = "";
-        String udf2 = "";
-        String udf3 = "";
-        String udf4 = "";
-        String udf5 = "";
-        String udf6 = "";
-        String udf7 = "";
-        String udf8 = "";
-        String udf9 = "";
-        String udf10 = "";
-        PayUmoneySdkInitializer.PaymentParam.Builder builder = new
-                PayUmoneySdkInitializer.PaymentParam.Builder();
-        builder.setAmount(1)                          // Payment amount
-                .setTxnId(txnId)                                             // Transaction ID
-                .setPhone(phone)                                           // User Phone number
-                .setProductName(productName)                   // Product Name or description
-                .setFirstName(firstName)                              // User First name
-                .setEmail(email)                                            // User Email ID
-                .setsUrl("https://www.payumoney.com/mobileapp/payumoney/success.php")                    // Success URL (surl)
-                .setfUrl("https://www.payumoney.com/mobileapp/payumoney/failure.php")                     //Failure URL (furl)
-                .setUdf1("")
-                .setUdf2("")
-                .setUdf3("")
-                .setUdf4("")
-                .setUdf5("")
-                .setUdf6("")
-                .setUdf7("")
-                .setUdf8("")
-                .setUdf9("")
-                .setUdf10("")
-                .setIsDebug(false)                              // Integration environment - true (Debug)/ false(Production)
-                .setKey("j0LyWpPK")                        // Merchant key
-                .setMerchantId("6089738");             // Merchant ID
+        if (!checkout_textinputlayout.getEditText().getText().toString().isEmpty()) {
+            String txnId = System.currentTimeMillis() + "";
+            String phone = "9582054664";
+            String productName = "Coins";
+            String firstName = "Pulkit Aggarwal";
+            String email = "aggarwalpulkit596@gmail.com";
+            String udf1 = "";
+            String udf2 = "";
+            String udf3 = "";
+            String udf4 = "";
+            String udf5 = "";
+            String udf6 = "";
+            String udf7 = "";
+            String udf8 = "";
+            String udf9 = "";
+            String udf10 = "";
+            amount = Integer.valueOf(checkout_textinputlayout.getEditText().getText().toString());
+            checkout_textinputlayout.setError(amount + "is equal to " + amount * 5 + " coins");
+            PayUmoneySdkInitializer.PaymentParam.Builder builder = new
+                    PayUmoneySdkInitializer.PaymentParam.Builder();
+            builder.setAmount(amount)                          // Payment amount
+                    .setTxnId(txnId)                                             // Transaction ID
+                    .setPhone(phone)                                           // User Phone number
+                    .setProductName(productName)                   // Product Name or description
+                    .setFirstName(firstName)                              // User First name
+                    .setEmail(email)                                            // User Email ID
+                    .setsUrl("https://www.payumoney.com/mobileapp/payumoney/success.php")                    // Success URL (surl)
+                    .setfUrl("https://www.payumoney.com/mobileapp/payumoney/failure.php")                     //Failure URL (furl)
+                    .setUdf1("")
+                    .setUdf2("")
+                    .setUdf3("")
+                    .setUdf4("")
+                    .setUdf5("")
+                    .setUdf6("")
+                    .setUdf7("")
+                    .setUdf8("")
+                    .setUdf9("")
+                    .setUdf10("")
+                    .setIsDebug(false)                              // Integration environment - true (Debug)/ false(Production)
+                    .setKey("j0LyWpPK")                        // Merchant key
+                    .setMerchantId("6089738");             // Merchant ID
 
-        mPaymentParams = builder.build();
+            mPaymentParams = builder.build();
 
-        mPaymentParams = calculateServerSideHashAndInitiatePayment1(mPaymentParams);
+            mPaymentParams = calculateServerSideHashAndInitiatePayment1(mPaymentParams);
 
-        PayUmoneyFlowManager.startPayUMoneyFlow(mPaymentParams, this, R.style.AppTheme_default, false);
+            PayUmoneyFlowManager.startPayUMoneyFlow(mPaymentParams, this, R.style.AppTheme_default, false);
+        } else {
+            checkout_textinputlayout.setError("Cant Be Empty");
+        }
 
 
     }
@@ -167,7 +179,7 @@ public class CheckoutActivity extends AppCompatActivity {
             // Check which object is non-null
             if (transactionResponse != null && transactionResponse.getPayuResponse() != null) {
                 if (transactionResponse.getTransactionStatus().equals(TransactionResponse.TransactionStatus.SUCCESSFUL)) {
-                    myRootref.child("coins").setValue(Coins + 5);
+                    myRootref.child("coins").setValue(Coins + amount*5);
                 } else {
                     //Failure Transaction
                 }
--- app/src/main/java/com/learnacad/learnacad/Fragments/MyReferals_Fragment.java
+++ app/src/main/java/com/learnacad/learnacad/Fragments/MyReferals_Fragment.java
@@ -4,25 +4,32 @@ package com.learnacad.learnacad.Fragments;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.learnacad.learnacad.Activities.CheckoutActivity;
 import com.learnacad.learnacad.Models.Student;
 import com.learnacad.learnacad.R;
 import com.orm.SugarRecord;
+
 import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -48,13 +55,20 @@ public class MyReferals_Fragment extends Fragment {
     @BindView(R.id.referal_addmorecoin)
     TextView addmorecoins;
 
-    DatabaseReference myRootref;
+    DatabaseReference userRef;
+    DatabaseReference mRootRef;
     Integer Coins;
     Student student;
 
 
     String referal_code;
 
+    @BindView(R.id.referal_textinputlayout)
+    TextInputLayout referal_input;
+
+    @BindView(R.id.referal_checkbutton)
+    Button referal_checkbutton;
+    String myParentnode;
 
 
     public MyReferals_Fragment() {
@@ -76,14 +90,14 @@ public class MyReferals_Fragment extends Fragment {
         pDialog.show();
 
 
-
         getActivity().setTitle("My Referals");
         List<Student> students = SugarRecord.listAll(Student.class);
         if (students != null && students.size() > 0)
             student = students.get(0);
-        myRootref = FirebaseDatabase.getInstance().getReference("users/"+student.getMobileNum());
+        userRef = FirebaseDatabase.getInstance().getReference("users/" + student.getMobileNum());
+        mRootRef = FirebaseDatabase.getInstance().getReference("users");
 
-        myRootref.addValueEventListener(new ValueEventListener() {
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Coins = dataSnapshot.child("coins").getValue(Integer.class);
@@ -119,9 +133,95 @@ public class MyReferals_Fragment extends Fragment {
         addmorecoins.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getContext(),CheckoutActivity.class);
-                i.putExtra("studentid",student.getMobileNum());
+                Intent i = new Intent(getContext(), CheckoutActivity.class);
+                i.putExtra("studentid", student.getMobileNum());
                 startActivity(i);
+            }
+        });
+
+        referal_checkbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String code = referal_input.getEditText().getText().toString();
+                final SweetAlertDialog pDialog = new SweetAlertDialog(getActivity(), SweetAlertDialog.PROGRESS_TYPE);
+                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog.setTitleText("Loading");
+                pDialog.setCancelable(false);
+                pDialog.show();
+                final Query userQuery = mRootRef.orderByChild("usedReferalCode").equalTo(code);
+
+                userQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.getValue() != null) {
+                            Log.i("TAG", "onDataChange: "+dataSnapshot);
+                            userQuery.addChildEventListener(new ChildEventListener() {
+                                @Override
+                                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                                    myParentnode = dataSnapshot.getKey();
+                                    Log.i("TAG", "onDataChange: "+dataSnapshot);
+                                    mRootRef.child(myParentnode).addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            Log.i("TAG", "onDataChange: " + dataSnapshot.child("usedReferalCode").getValue(String.class) + code);
+                                            if (code.equals(dataSnapshot.child("usedReferalCode").getValue(String.class))) {
+                                                userRef.child("coins").setValue(Coins + 50);
+                                                mRootRef.child(myParentnode).child("coins").setValue(dataSnapshot.child("coins").getValue(Integer.class) + 50);
+                                                pDialog.dismissWithAnimation();
+                                                referal_input.setVisibility(View.GONE);
+                                                referal_checkbutton.setVisibility(View.GONE);
+
+                                            } else {
+                                                referal_input.setErrorEnabled(false);
+                                                referal_input.setError("Invalid Code");
+                                                pDialog.dismissWithAnimation();
+
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                                }
+
+                                @Override
+                                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                                }
+
+                                @Override
+                                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+
+                        } else {
+                            referal_input.setErrorEnabled(false);
+                            referal_input.setError("Invalid Code");
+                            pDialog.dismissWithAnimation();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
             }
         });
 
@@ -131,14 +231,11 @@ public class MyReferals_Fragment extends Fragment {
     }
 
 
-
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
     }
-
 
 
 }
